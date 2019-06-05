d545d32b09d258984e7a22d5ec6ba03fe2f885ca
OasisArtisan <omaralama31@yahoo.com>
2019-06-05 14:43:07 +0300

    Finish generate report code

--- core.py
+++ core.py
@@ -4,26 +4,20 @@ import copy
 import schedulers
 import math
 
+
 class Job(simpy.Event):
-    def __init__(self, env, units, source=None, name=None, tag=None, priority=None):
+    def __init__(self, env, units, **extras):
         super().__init__(env)
         self.env = env
         self.units = units
         self.remaining_units = units
-        self.source = source
-        self.name = name
-        self.tag = tag
-        self.priority = priority
-        self._processor = None
+        self.extras = extras
 
     def __str__(self):
-        return "{}{}{}{}{}".format(
-            "Name: {:<10} ".format(self.name) if self.name is not None else "",
-            "Tag: {:<3} ".format(self.tag) if self.tag is not None else "",
-            "Src: {:<10} ".format(self.source) if self.source is not None else "",
-            "Units: {:2}/{:<2} ".format(self.units - self.remaining_units, self.units),
-            "Priority: {:<2} ".format(self.priority) if self.priority is not None else ""
-        )
+        s = "Units: {:2}/{:<2} ".format(self.units - self.remaining_units, self.units)
+        for item in self.extras.items():
+            s += "{}: {} ".format(item[0],item[1])
+        return s
 
 
 class ProcessingUnit:
@@ -67,25 +61,22 @@ class ProcessingUnit:
                     iteration_rate -= to_process
                     job.remaining_units -= to_process
                     if job.remaining_units == 0:
-                        will_finish_jobs.append(job)
+                        will_finish_jobs.append((job, to_process))
                         self.scheduler.remove(job)
                     else:
-                        current_job = job
+                        current_job = (job,to_process)
                 # Add utilization info
-                # We copy the jobs instead of passing a reference so that the state of the job at that time is preserved
-                # Not RAM efficient but its a simple simulator after all so we can get away with it
                 utilization_list = list()
                 for job in will_finish_jobs:
-                    utilization_list.append(copy.copy(job))
+                    utilization_list.append(job)
                 if current_job is not None:
-                    utilization_list.append(copy.copy(current_job))
+                    utilization_list.append(current_job)
                 if len(utilization_list) > 0:
-                    self.utilization[self.env.now] = {'%': (self.rate - iteration_rate)/self.rate,
-                                                      'jobs': utilization_list}
+                    self.utilization[self.env.now] = utilization_list
                 # Step
                 yield self.env.timeout(1)
                 # Finalize finished jobs
-                for job in will_finish_jobs:
+                for job,_ in will_finish_jobs:
                     job.succeed()  # Fire job finished event
                     if self.out_pipe is not None:
                         self.out_pipe.queue(job)
@@ -95,61 +86,6 @@ class ProcessingUnit:
         except AttributeError as e:
             print("[Error] Please make sure that you have mounted a valid scheduler on {}".format(self))
             raise e
-
-    def get_average_utilization(self, start=0, end=None, source=None, name=None, tag=None):
-        total = 0
-        for item in self.utilization.items():
-            t = item[0]
-            if t >= start and (end is None or t <= end):
-                total += item[1]['%']
-        return total/self.env.now
-
-    def generate_report(self, start=0, end=None,
-                        time_grouping=1, show_scaled_time=False,
-                        row_labels=None, cell_labels=None,
-                        show_column_labels=True, show_row_utilization=True, show_header=True,
-                        long_value_handling="trim"):
-        """
-        :param start: From which time step should we generate the report (Inclusive)
-        :param end: To which time step (Inclusive)
-        :param time_grouping: How many time steps per column. If set to 0 or
-        None then only one column is used regardless of time steps
-        :param show_scaled_time: If time_group=5 then a value of 3 means t=15 if this is set to true
-        :param row_labels: Group rows using none or all of ["source","name","tag"]. If an empty list is passed or None,
-        then one group is used for the whole processing unit
-        :param cell_labels: Label cells using none or all of ["source","name","tag"]. If an empty list is passed or None,
-        then the cell is simply marked as has job "X" or empty "-"
-        :param show_column_labels: Whether to show column labels (time steps) or not
-        :param show_row_utilization: Whether to print utilization percentage for every row (group)
-        :param show_header: Whether to print a header with the unit name and the average utilization of the resource
-        :param long_value_handling: "trim", "wrap", "push"
-        """
-
-        cell_width = 5
-        row_header_width = 10
-        report = []
-        duration = (env.now if end is None else end) - start
-        if not time_grouping:
-            time_scale = duration
-        scaled_start = math.floor(start/time_grouping)
-        scaled_end = math.ceil((env.now if end is None else end) / time_grouping)
-        grouped_time_steps = range(scaled_start, scaled_end)
-        avg_util = self.get_average_utilization(start, end)
-        if show_header:
-            report.append("{} Util: {:<3.2f}% -------------------------------------------".format(self, avg_util * 100))
-
-        if show_column_labels:
-            if show_scaled_time:
-                column_labels = "t({{:<{}}}): ".format(row_header_width-5).format(time_grouping)
-            else:
-                column_labels = "t{{:<{}}}: ".format(row_header_width-3).format(" ")
-            for i in grouped_time_steps:
-                column_labels += "{{:{}}}".format(cell_width).format(i if show_scaled_time else i*time_grouping)
-            report.append(column_labels)
-        # Generate groupings
-
-        return '\n'.join(report)
-
 
     def _print(self, msg, verbosity):
         if self._sim_printer:
@@ -163,17 +99,19 @@ if __name__ == '__main__':
     """
     An example usage of the processing unit
     """
-    from io_utils import SimPrinter
+    from io_utils import SimPrinter, generate_report
     import random
     env = simpy.Environment()
-    gpu = ProcessingUnit(env, rate=1, name="GPU:1", sim_printer=SimPrinter(verbosity=1).print)
+    gpu = ProcessingUnit(env, rate=1, name="GPU:1", sim_printer=SimPrinter(verbosity=0).print)
     gpu.mount_scheduler(schedulers.FIFOScheduler())
     gpu_process = env.process(gpu.main_process())
     for i in range(10):
-        job = Job(env,units=random.randint(0,10),name=i)
+        job = Job(env,units=random.randint(0,10), name=i, tag="Coco")
+        gpu.queue(job)
+        job = Job(env, units=random.randint(0, 10), name=i)
         gpu.queue(job)
     env.run(until=500)
-    print(gpu.generate_report(start=0,time_grouping=5))
+    print(generate_report(gpu, start=0,time_grouping=1, row_labels=["name","tag"]))
     # for item in gpu.utilization.items():
     #     s = [str(x) for x in item[1]['jobs']]
     #     print("t:{:<4}] Utilization: {:6}% Jobs: {}".format(item[0],item[1]['%']*100, s))
--- io_utils.py
+++ io_utils.py
@@ -1,3 +1,4 @@
+import math
 class SimPrinter:
     def __init__(self, verbosity=2):
         self.verbosity = verbosity
@@ -5,3 +6,113 @@ class SimPrinter:
     def print(self, env, source, msg, verbosity):
         if self.verbosity >= verbosity:
             print("t:{:<4}] {}] {}".format(env.now, source, msg))
+
+
+def generate_report(processing_unit, start=0, end=None,
+                    time_grouping=1, show_scaled_time=False,
+                    row_labels=None, cell_labels=None,
+                    show_column_labels=True, show_row_utilization=True, show_header=True,
+                    long_value_handling="trim"):
+    """
+    :param start: From which time step should we generate the report (Inclusive)
+    :param end: To which time step (Inclusive)
+    :param time_grouping: How many time steps per column. If set to 0 or
+    None then only one column is used regardless of time steps
+    :param show_scaled_time: If time_group=5 then a value of 3 means t=15 if this is set to true
+    :param row_labels: Group rows using none or a combination of the extras keys.
+    If an empty list is passed or None, then one group is used for the whole processing unit
+    :param cell_labels: Label cells using none or a combination of the extras keys.
+    If an empty list is passed or None, then the cell is simply marked as has job "X" or empty "-"
+    :param show_column_labels: Whether to show column labels (time steps) or not
+    :param show_row_utilization: Whether to print utilization percentage for every row (group)
+    :param show_header: Whether to print a header with the unit name and the average utilization of the resource
+    :param long_value_handling: "trim", "wrap", "push"
+    """
+    cell_width = 5
+    row_header_width = 30
+    report = []
+    duration = (processing_unit.env.now if end is None else end) - start
+    if not time_grouping:
+        time_scale = duration
+    scaled_start = int(math.floor(start/time_grouping))
+    scaled_end = int(math.ceil((processing_unit.env.now if end is None else end) / time_grouping))
+    grouped_time_steps = range(scaled_start, scaled_end)
+    avg_util = 0
+    # Generate header
+    if show_header:
+        report.append("{} Util: {:<3.2f}% -------------------------------------------".format(processing_unit, avg_util * 100))
+    # Generate column labels
+    if show_column_labels:
+        if show_scaled_time:
+            column_labels = "t({{:<{}}})|".format(row_header_width-3).format(time_grouping)
+        else:
+            column_labels = "t{{:<{}}}|".format(row_header_width-1).format("")
+        for i in grouped_time_steps:
+            column_labels += "{{:<{}}}|".format(cell_width).format(i if show_scaled_time else i*time_grouping)
+        report.append(column_labels)
+    # Generate groups
+    groups = set()
+    if row_labels is not None and len(row_labels) > 0:
+        for jobs in processing_unit.utilization.values():
+            for job, _ in jobs:
+                values = list()
+                for key in row_labels:
+                    # We add a true/false flag before the value so that the sorting algorithm can sort None values
+                    if key in job.extras.keys():
+                        values.append(job.extras[key])
+                    else:
+                        # Add null character to signify that the key is missing
+                        values.append(chr(0))
+                groups.add(tuple(values))
+    # Sort groups
+    groups = list(groups)
+    groups.sort(key=lambda x: [str(y) for y in x])
+    # Generate group rows
+    for group in groups:
+        # Generate row header
+        row_header = ""
+        for i, key in enumerate(row_labels):
+            row_header += "{}:{} ".format(key, group[i])
+        # Generate row body
+        row_units = 0
+        cells = list()
+        for tg in grouped_time_steps:
+            cell_units = 0
+            jobs = []
+            # Generate cell
+            for t in range(tg,tg+time_grouping):
+                if t in processing_unit.utilization.keys():
+                    for job, units in processing_unit.utilization[t]:
+                        include = True
+                        for i, key in enumerate(row_labels):
+                            if not (key in job.extras.keys() and job.extras[key] == group[i]) and \
+                                    not (key not in job.extras.keys() and group[i] == str(chr(0))):
+                                include = False
+                                break
+
+                        if include:
+                            cell_units += units
+                            jobs.append(job)
+            row_units += cell_units
+            if cell_labels is not None and len(cell_labels) > 0:
+                jobs_text = list()
+                for job in jobs:
+                    values = list()
+                    for key in cell_labels:
+                        if key in job.extras:
+                            values.append(str(job.extras[key]))
+                        else:
+                            values.append("")
+                    jobs_text.append(":".join(values))
+                cells.append("{{:{}}}".format(cell_width).format(','.join(jobs_text)))
+            else:
+                if len(jobs) > 0:
+                    cells.append("X"*cell_width)
+                else:
+                    cells.append("-"*cell_width)
+        row_util = row_units / (processing_unit.rate * duration)
+        if show_row_utilization:
+            row_header += "({:<3.2f}%) ".format(row_util*100)
+        row_header = "{{:{}}}|".format(row_header_width).format(row_header)
+        report.append(row_header + '|'.join(cells) + '|')
+    return '\n'.join(report)
